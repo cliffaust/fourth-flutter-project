fa7a96bb457b91df3d6db942a308c0b6ae03a02f
Clifford <kingsleyclifford2016@gmail.com>
2021-11-21 18:45:58 +0000

    added lines of code

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 
 class Loading extends StatefulWidget {
@@ -12,9 +13,18 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   void getData() async {
-    Response response = await get(Uri.parse("https://jsonplaceholder.typicode.com/todos/1"));
+    Response response = await get(Uri.parse("http://worldtimeapi.org/api/timezone/Europe/London"));
 
-    print(response.body);
+    Map data = jsonDecode(response.body);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    DateTime now = DateTime.parse(datetime);
+
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    print(now);
   }
 
   @override
